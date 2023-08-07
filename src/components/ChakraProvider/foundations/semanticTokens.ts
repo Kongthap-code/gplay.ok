04ead4e130ef78d4ab78edb843daf8f8d797d203
Kongthap phuengsang <sisoopth@hotmail.com>
2023-08-08 00:29:14 +0700

    fix color page

--- src/components/ChakraProvider/foundations/semanticTokens.ts
+++ src/components/ChakraProvider/foundations/semanticTokens.ts
@@ -1,10 +1,10 @@
 const semanticTokens = {
     colors: {
-        "primary-color1": '#fde50d',
-        "primary-color8": '#feee5e',
-        "primary-color6": '#fef287',
-        "primary-color4": '#fef6b0',
-        "primary-color2": '#fffce7',
+        "primary-color1": '#008f6b',
+        "primary-color8": '#33a589',
+        "primary-color6": '#66bca6',
+        "primary-color4": '#b3ddd3',
+        "primary-color2": '#e6f4f0',
         "secondary-color1": '#fde50d',
         "secondary-color8": '#feee5e',
         "secondary-color6": '#fef287',
